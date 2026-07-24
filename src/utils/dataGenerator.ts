da66af98427c264be4d8f9e541a7391e8d869cb2
habebz20001-sys <habebz20001@gmail.com>
2026-07-24 13:06:51 +0300

    feat: implement DataGenerator utility for test data

--- src/utils/dataGenerator.ts
+++ src/utils/dataGenerator.ts
@@ -1,20 +1,75 @@
 import { faker } from '@faker-js/faker';
 
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  company: string;
+  address: string;
+  state: string;
+  city: string;
+  zipCode: string;
+  mobileNumber: string;
+}
+
 export class DataGenerator {
-    static generateUser() {
-        return {
-            name: faker.person.firstName(),
-            email: faker.internet.email(),
-            password: faker.internet.password({ length: 10 }),
-            firstName: faker.person.firstName(),
-            lastName: faker.person.lastName(),
-            company: faker.company.name(),
-            address: faker.location.streetAddress(),
-            country: 'United States',
-            state: faker.location.state(),
-            city: faker.location.city(),
-            zipcode: faker.location.zipCode(),
-            mobileNumber: faker.phone.number(),
-        };
-    }
+  /**
+   * Generate a random user with all required details
+   */
+  static generateUser(): User {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+
+    return {
+      name: `${firstName} ${lastName}`,
+      email: faker.internet.email().toLowerCase(),
+      password: faker.internet.password({
+        length: 12,
+        memorable: false,
+        pattern: /[A-Za-z0-9!@#$%^&*]/,
+      }),
+      firstName: firstName,
+      lastName: lastName,
+      company: faker.company.name(),
+      address: faker.location.streetAddress(),
+      state: faker.location.state(),
+      city: faker.location.city(),
+      zipCode: faker.location.zipCode(),
+      mobileNumber: faker.phone.number('+1##########'),
+    };
+  }
+
+  /**
+   * Generate a random email
+   */
+  static generateEmail(): string {
+    return faker.internet.email().toLowerCase();
+  }
+
+  /**
+   * Generate a random password
+   */
+  static generatePassword(): string {
+    return faker.internet.password({
+      length: 12,
+      memorable: false,
+      pattern: /[A-Za-z0-9!@#$%^&*]/,
+    });
+  }
+
+  /**
+   * Generate random product name
+   */
+  static generateProductName(): string {
+    return faker.commerce.productName();
+  }
+
+  /**
+   * Generate random message
+   */
+  static generateMessage(length: number = 100): string {
+    return faker.lorem.paragraph(length);
+  }
 }
