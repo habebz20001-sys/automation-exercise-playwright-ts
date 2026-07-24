465ceb8ea217d78d4c5038d70a6a82c545146eb7
habebz20001-sys <habebz20001@gmail.com>
2026-07-24 13:08:01 +0300

    fix: update playwright config, add env example, fix gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 :1,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://automationexercise.com',
+    baseURL: process.env.BASE_URL || 'https://automationexercise.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -37,6 +37,10 @@ export default defineConfig({
     video: 'retain-on-failure',
   },
 
+  timeout: 60000,
+  expect: {
+    timeout: 10000,
+  },
 
   /* Configure projects for major browsers */
   projects: [
